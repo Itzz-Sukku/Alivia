891a7beee3ecc5ed8a22bf20abd041b3e5a327b0
Itzz-Sukku <iamistkhar78@gmail.com>
2026-01-18 22:35:46 +0530

    Update broadcast.py

--- AnonXMusic/plugins/misc/broadcast.py
+++ AnonXMusic/plugins/misc/broadcast.py
@@ -1,8 +1,6 @@
 import asyncio
-import os
-import time
+
 from pyrogram import filters
-from pyrogram.types import Message
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
@@ -17,14 +15,14 @@ from AnonXMusic.utils.database import (
 )
 from AnonXMusic.utils.decorators.language import language
 from AnonXMusic.utils.formatters import alpha_to_int
-from config import adminlist, CACHE_DURATION, CACHE_SLEEP ,file_cache, autoclean
+from config import adminlist
 
 IS_BROADCASTING = False
 
 
 @app.on_message(filters.command("broadcast") & SUDOERS)
 @language
-async def braodcast_message(client, message:Message, _):
+async def braodcast_message(client, message, _):
     global IS_BROADCASTING
     if message.reply_to_message:
         x = message.reply_to_message.id
@@ -167,29 +165,5 @@ async def auto_clean():
         except:
             continue
 
-
-async def auto_clean_cache():
-    """Periodically clean up expired files"""
-    while not await asyncio.sleep(CACHE_SLEEP):
-        try:
-            current_time = time.time()
-            expired_files = [
-                file_path
-                for file_path, last_access in file_cache.items()
-                if current_time - last_access > CACHE_DURATION
-                and file_path not in autoclean
-            ]
-            
-            for file_path in expired_files:
-                try:
-                    if os.path.exists(file_path):
-                        os.remove(file_path)
-                        file_cache.pop(file_path, None)
-                except:
-                    continue
-        except:
-            continue
-
-asyncio.create_task(auto_clean_cache())
 
 asyncio.create_task(auto_clean())
